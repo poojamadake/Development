3f83620086f625054aed2a611b58f965263fc237
poojamadake <106139386+poojamadake@users.noreply.github.com>
2022-05-24 23:32:09 +0530

    Update Array1.c

--- Array1.c
+++ Array1.c
@@ -4,13 +4,8 @@ int main()
 {
     int no = 11;
 
-
-    // Array defination
     int Arr[5];
-    // Arr is one dimentional array , which contains 5 element , and each elelemnt is of type integer.
-
-    // Array member initialization
-    // Member by member initialization
+   
     Arr[0] = 11;
     Arr[1] = 21;
     Arr[2] = 51;
@@ -18,17 +13,9 @@ int main()
     Arr[4] = 111;
 
 
-
-
-
-
-
-   // Member initialization list
    int Brr[5] = {11,21,51,101,111};
 
    int Crr[ ] = {11,21,51,101,111};
-
-   // int Drr[];    Error
     return 0;
 }
 
