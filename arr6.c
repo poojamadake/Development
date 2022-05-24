31feaad3576983a532cdadbdfbf3dda3180ecaf0
poojamadake <106139386+poojamadake@users.noreply.github.com>
2022-05-24 23:35:46 +0530

    Update arr6.c

--- arr6.c
+++ arr6.c
@@ -4,15 +4,15 @@ int main()
 {
     int Arr[5] = {10,20,30,40,50};
 
-    printf("%d\n",Arr[0]);      // 10
-    printf("%d\n",Arr[4]);      // 50
-    printf("%d\n",Arr);         // 100
-    printf("%d\n",&Arr);        // 100
-    printf("%d\n",&(Arr[0]));       // 100
-    printf("%d\n",Arr + 1);     // 104
-    printf("%d\n",(&Arr) + 1);  // 120
-    printf("%d\n",sizeof(Arr));     // 20
-    printf("%d\n",sizeof(Arr[2]));  //
+    printf("%d\n",Arr[0]);      
+    printf("%d\n",Arr[4]);      
+    printf("%d\n",Arr);         
+    printf("%d\n",&Arr);        
+    printf("%d\n",&(Arr[0]));       
+    printf("%d\n",Arr + 1);     
+    printf("%d\n",(&Arr) + 1);  
+    printf("%d\n",sizeof(Arr));     
+    printf("%d\n",sizeof(Arr[2]));  
     printf("%d\n",Arr[2]);
     printf("%d\n",*(Arr+2));
     printf("%d\n",*(2+Arr));
@@ -20,33 +20,6 @@ int main()
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-1                    Arr[2];                     30
-
-
-2                    *(Arr + 2)              *(100 + 2)              *(108)          30
-
-
-3                   *(2 + Arr)              *(2 + 100)              *(108)          30
-
-
-4                      2[Arr]                 30
-
-
-*/
-
 
 
 
